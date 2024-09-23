9ba6627bd7f15ba9eeaaa7f992ed3803ef014f54
Rui <rui95fer@gmail.com>
2024-09-23 21:57:07 +0100

    prettier added

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -1,5 +1,5 @@
 <script setup>
-import {Head, Link} from '@inertiajs/vue3';
+import { Head, Link } from "@inertiajs/vue3";
 
 defineProps({
     canLogin: {
@@ -17,23 +17,23 @@ defineProps({
         required: true,
     },
     name: {
-        type: String
+        type: String,
     },
     time: {
-        type: String
-    }
+        type: String,
+    },
 });
 
 function handleImageError() {
-    document.getElementById('screenshot-container')?.classList.add('!hidden');
-    document.getElementById('docs-card')?.classList.add('!row-span-1');
-    document.getElementById('docs-card-content')?.classList.add('!flex-row');
-    document.getElementById('background')?.classList.add('!hidden');
+    document.getElementById("screenshot-container")?.classList.add("!hidden");
+    document.getElementById("docs-card")?.classList.add("!row-span-1");
+    document.getElementById("docs-card-content")?.classList.add("!flex-row");
+    document.getElementById("background")?.classList.add("!hidden");
 }
 </script>
 
 <template>
-    <Head title="Welcome"/>
+    <Head title="Welcome" />
     <div class="bg-gray-50 text-black/50 dark:bg-black dark:text-white/50">
         <img
             id="background"
@@ -44,7 +44,9 @@ function handleImageError() {
             class="relative min-h-screen flex flex-col items-center justify-center selection:bg-[#FF2D20] selection:text-white"
         >
             <div class="relative w-full max-w-2xl px-6 lg:max-w-7xl">
-                <header class="grid grid-cols-2 items-center gap-2 py-10 lg:grid-cols-3">
+                <header
+                    class="grid grid-cols-2 items-center gap-2 py-10 lg:grid-cols-3"
+                >
                     <div class="flex lg:justify-center lg:col-start-2">
                         <svg
                             class="h-12 w-auto text-white lg:h-16 lg:text-[#FF2D20]"
@@ -93,7 +95,10 @@ function handleImageError() {
                             id="docs-card"
                             class="flex flex-col items-start gap-6 overflow-hidden rounded-lg bg-white p-6 shadow-[0px_14px_34px_0px_rgba(0,0,0,0.08)] ring-1 ring-white/[0.05] transition duration-300 hover:text-black/70 hover:ring-black/20 focus:outline-none focus-visible:ring-[#FF2D20] md:row-span-3 lg:p-10 lg:pb-10 dark:bg-zinc-900 dark:ring-zinc-800 dark:hover:text-white/70 dark:hover:ring-zinc-700 dark:focus-visible:ring-[#FF2D20]"
                         >
-                            <div id="screenshot-container" class="relative flex w-full flex-1 items-stretch">
+                            <div
+                                id="screenshot-container"
+                                class="relative flex w-full flex-1 items-stretch"
+                            >
                                 <img
                                     src="https://laravel.com/assets/img/welcome/docs-light.svg"
                                     alt="Laravel documentation screenshot"
@@ -110,8 +115,13 @@ function handleImageError() {
                                 ></div>
                             </div>
 
-                            <div class="relative flex items-center gap-6 lg:items-end">
-                                <div id="docs-card-content" class="flex items-start gap-6 lg:flex-col">
+                            <div
+                                class="relative flex items-center gap-6 lg:items-end"
+                            >
+                                <div
+                                    id="docs-card-content"
+                                    class="flex items-start gap-6 lg:flex-col"
+                                >
                                     <div
                                         class="flex size-12 shrink-0 items-center justify-center rounded-full bg-[#FF2D20]/10 sm:size-16"
                                     >
@@ -133,12 +143,20 @@ function handleImageError() {
                                     </div>
 
                                     <div class="pt-3 sm:pt-5 lg:pt-0">
-                                        <h2 class="text-xl font-semibold text-black dark:text-white">Documentation</h2>
+                                        <h2
+                                            class="text-xl font-semibold text-black dark:text-white"
+                                        >
+                                            Documentation
+                                        </h2>
 
                                         <p class="mt-4 text-sm/relaxed">
-                                            Laravel has wonderful documentation covering every aspect of the framework.
-                                            Whether you are a newcomer or have prior experience with Laravel, we
-                                            recommend reading our documentation from beginning to end.
+                                            Laravel has wonderful documentation
+                                            covering every aspect of the
+                                            framework. Whether you are a
+                                            newcomer or have prior experience
+                                            with Laravel, we recommend reading
+                                            our documentation from beginning to
+                                            end.
                                         </p>
                                     </div>
                                 </div>
@@ -181,11 +199,17 @@ function handleImageError() {
                             </div>
 
                             <div class="pt-3 sm:pt-5">
-                                <h2 class="text-xl font-semibold text-black dark:text-white">Laracasts</h2>
+                                <h2
+                                    class="text-xl font-semibold text-black dark:text-white"
+                                >
+                                    Laracasts
+                                </h2>
 
                                 <p class="mt-4 text-sm/relaxed">
-                                    Laracasts offers thousands of video tutorials on Laravel, PHP, and JavaScript
-                                    development. Check them out, see for yourself, and massively level up your
+                                    Laracasts offers thousands of video
+                                    tutorials on Laravel, PHP, and JavaScript
+                                    development. Check them out, see for
+                                    yourself, and massively level up your
                                     development skills in the process.
                                 </p>
                             </div>
@@ -233,12 +257,18 @@ function handleImageError() {
                             </div>
 
                             <div class="pt-3 sm:pt-5">
-                                <h2 class="text-xl font-semibold text-black dark:text-white">Laravel News</h2>
+                                <h2
+                                    class="text-xl font-semibold text-black dark:text-white"
+                                >
+                                    Laravel News
+                                </h2>
 
                                 <p class="mt-4 text-sm/relaxed">
-                                    Laravel News is a community driven portal and newsletter aggregating all of the
-                                    latest and most important news in the Laravel ecosystem, including new package
-                                    releases and tutorials.
+                                    Laravel News is a community driven portal
+                                    and newsletter aggregating all of the latest
+                                    and most important news in the Laravel
+                                    ecosystem, including new package releases
+                                    and tutorials.
                                 </p>
                             </div>
 
@@ -278,66 +308,72 @@ function handleImageError() {
                             </div>
 
                             <div class="pt-3 sm:pt-5">
-                                <h2 class="text-xl font-semibold text-black dark:text-white">Vibrant Ecosystem</h2>
+                                <h2
+                                    class="text-xl font-semibold text-black dark:text-white"
+                                >
+                                    Vibrant Ecosystem
+                                </h2>
 
                                 <p class="mt-4 text-sm/relaxed">
-                                    Laravel's robust library of first-party tools and libraries, such as
+                                    Laravel's robust library of first-party
+                                    tools and libraries, such as
                                     <a
                                         href="https://forge.laravel.com"
                                         class="rounded-sm underline hover:text-black focus:outline-none focus-visible:ring-1 focus-visible:ring-[#FF2D20] dark:hover:text-white dark:focus-visible:ring-[#FF2D20]"
-                                    >Forge</a
+                                        >Forge</a
                                     >,
                                     <a
                                         href="https://vapor.laravel.com"
                                         class="rounded-sm underline hover:text-black focus:outline-none focus-visible:ring-1 focus-visible:ring-[#FF2D20] dark:hover:text-white"
-                                    >Vapor</a
+                                        >Vapor</a
                                     >,
                                     <a
                                         href="https://nova.laravel.com"
                                         class="rounded-sm underline hover:text-black focus:outline-none focus-visible:ring-1 focus-visible:ring-[#FF2D20] dark:hover:text-white"
-                                    >Nova</a
+                                        >Nova</a
                                     >,
                                     <a
                                         href="https://envoyer.io"
                                         class="rounded-sm underline hover:text-black focus:outline-none focus-visible:ring-1 focus-visible:ring-[#FF2D20] dark:hover:text-white"
-                                    >Envoyer</a
+                                        >Envoyer</a
                                     >, and
                                     <a
                                         href="https://herd.laravel.com"
                                         class="rounded-sm underline hover:text-black focus:outline-none focus-visible:ring-1 focus-visible:ring-[#FF2D20] dark:hover:text-white"
-                                    >Herd</a
+                                        >Herd</a
                                     >
-                                    help you take your projects to the next level. Pair them with powerful open source
+                                    help you take your projects to the next
+                                    level. Pair them with powerful open source
                                     libraries like
                                     <a
                                         href="https://laravel.com/docs/billing"
                                         class="rounded-sm underline hover:text-black focus:outline-none focus-visible:ring-1 focus-visible:ring-[#FF2D20] dark:hover:text-white"
-                                    >Cashier</a
+                                        >Cashier</a
                                     >,
                                     <a
                                         href="https://laravel.com/docs/dusk"
                                         class="rounded-sm underline hover:text-black focus:outline-none focus-visible:ring-1 focus-visible:ring-[#FF2D20] dark:hover:text-white"
-                                    >Dusk</a
+                                        >Dusk</a
                                     >,
                                     <a
                                         href="https://laravel.com/docs/broadcasting"
                                         class="rounded-sm underline hover:text-black focus:outline-none focus-visible:ring-1 focus-visible:ring-[#FF2D20] dark:hover:text-white"
-                                    >Echo</a
+                                        >Echo</a
                                     >,
                                     <a
                                         href="https://laravel.com/docs/horizon"
                                         class="rounded-sm underline hover:text-black focus:outline-none focus-visible:ring-1 focus-visible:ring-[#FF2D20] dark:hover:text-white"
-                                    >Horizon</a
+                                        >Horizon</a
                                     >,
                                     <a
                                         href="https://laravel.com/docs/sanctum"
                                         class="rounded-sm underline hover:text-black focus:outline-none focus-visible:ring-1 focus-visible:ring-[#FF2D20] dark:hover:text-white"
-                                    >Sanctum</a
+                                        >Sanctum</a
                                     >,
                                     <a
                                         href="https://laravel.com/docs/telescope"
                                         class="rounded-sm underline hover:text-black focus:outline-none focus-visible:ring-1 focus-visible:ring-[#FF2D20] dark:hover:text-white"
-                                    >Telescope</a
+                                        >Telescope</a
                                     >, and more.
                                 </p>
                             </div>
@@ -345,11 +381,13 @@ function handleImageError() {
                     </div>
                 </main>
 
-                <footer class="py-16 text-center text-sm text-black dark:text-white/70">
+                <footer
+                    class="py-16 text-center text-sm text-black dark:text-white/70"
+                >
                     Laravel v{{ laravelVersion }} (PHP v{{ phpVersion }})
-                    <br>
+                    <br />
                     {{ name }}
-                    <br>
+                    <br />
                     {{ time }}
                 </footer>
             </div>
